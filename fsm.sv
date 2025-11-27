840b3574f549281112f494f3be670e2ba674f3e3
Ntalas11 <christospapazacharias@outlook.com>
2025-11-27 21:50:03 +0000

    testing

--- fsm.sv
+++ fsm.sv
@@ -2,6 +2,7 @@ module f1fsm (
 	input logic sysclk,
 	input logic tick,
 	input logic trigger,
+	input logic time_out;
 	output logic en_lfsr,
 	output logic start_delay,
 	output logic [9:0] ledr
@@ -48,30 +49,26 @@ default:next_state =IDLE;
 
 endcase
 
-always_comb
-case (count)
-
-1: ledr[0] = 1;
-2: ledr[1] = 1;
-3: ledr[2] = 1;
-4: ledr[3] = 1;
-5: ledr[4] = 1;
-6: ledr[5] = 1;
-7: ledr[6] = 1;
-8: ledr[7] = 1;
-9: ledr[8] = 1;
-10:ledr[9] = 1;
-default: ledr = 0;
-endcase
 
 
 
-always_ff @(posedge sysclk && tick) begin
-if ((count < 10) && (current_state == COUNT))
-count <= count + 1;
-else count <= 0 ;
- timeout <=1 ;
+
+always_ff @(posedge sysclk) begin
+    if (tick) begin
+        
+
+        if (current_state == COUNT) begin
+            if (count < 10) begin
+                count <= count + 1;
+                ledr[count] <= 1;
+            end else begin
+                count   <= 0;
+                timeout <= 1;
+            end
+        end
+    end
 end
+
 
 
 
